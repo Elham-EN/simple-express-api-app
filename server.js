9296c1a455e449c9e1fcfb06ad1aa2c2db0e3227
Elham-EN <ilhamrezaie@gmail.com>
2022-01-16 19:24:53 +1100

    Templating Engine

--- server.js
+++ server.js
@@ -6,22 +6,19 @@ const PORT = 3000;
 //Express Application
 const app = express();
 
+//Where we can set the view engine, that is the name of our templating
+//engine like hbs.
+app.set("view engine", "hbs");
+//And the view which points to a directory where the application view can
+//be found. Pointing view to the path of our views folder
+app.set("views", path.join(__dirname, "views"));
+
 //Middleware functions
 app.use(function (req, res, next) {
   console.log(`Method: ${req.method} URL: ${req.url}`);
   next(); //pass control next middleware function
 });
 
-//Serving static page. The static() func input is a string containing a
-//relative path of the folder that we want to make available from our server
-//Make these static files available to route "/site".
-//The path of this folder is relative. Specifcally relative to the folder from
-//which you launch your node application
-//app.use("/site", express.static("public"));
-
-//Now static middleware is looking at the absolute path starting from the
-//directory where server.js lives, which is the root of our project and then
-//only from there looking into the public folder
 app.use("/site", express.static(path.join(__dirname, "public")));
 
 app.use(express.json());
@@ -34,6 +31,13 @@ app.use(function (req, res, next) {
 app.use("/friends", friendsRouter);
 
 app.use("/messages", messagesRouter);
+
+app.get("/", (req, res) => {
+  //Tell express that we're rendering the handlebars file called
+  //index.hbs and the second arg is an object which has the values
+  //for all of those variables in our hanldebars file.
+  res.render("index", { title: "I like snow mountain", caption: "Let's go skiing!" });
+});
 
 app.listen(PORT, () => {
   console.log("====================================");
